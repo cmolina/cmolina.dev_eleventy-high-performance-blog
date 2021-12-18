7e182d6a7ffbc3f55cf11bf358bce870d3ab48d8
Carlos Molina A <hi@cmolina.dev>
2021-12-18 12:33:55 -0300

    fix: Rollback to embedded asciinema player

--- _11ty/asciinema.js
+++ _11ty/asciinema.js
@@ -1,10 +1,6 @@
 module.exports = function(id, title) {
-    const params = "autoplay=1&loop=1&cols=112";
-    const src = `https://asciinema.org/a/${id}/embed?${params}`;
     return `
-<div id="asciicast-container-${id}" class="asciicast" style="display: block; float: none; overflow: hidden; padding: 0px; margin: 20px 0px;">
-<iframe src="${src}" title="${title}" id="asciicast-iframe-${id}" name="asciicast-iframe-${id}" scrolling="no" allow="fullscreen" style="border: 0;" hidden></iframe>
-<noscript><img src="https://asciinema.org/a/${id}.png"></noscript>
-</div>
+<script id="asciicast-${id}" src="https://asciinema.org/a/${id}.js" async data-autoplay="1" data-loop="1"></script>
+<noscript><img src="https://asciinema.org/a/${id}.svg" alt="${title}"></noscript>
 `;
 };
